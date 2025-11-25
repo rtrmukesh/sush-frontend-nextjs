2e8b7bfa487ef01ebdde3364b880c5b605be2d68
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2025-11-25 16:59:56 +0530

    deploy error fixed

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,15 +1,20 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_SECRET = process.env.JWT_SECRET;
+if (!JWT_SECRET) throw new Error("JWT_SECRET not defined");
 
-export const generateToken = (payload: object, expiresIn: string = "4h") => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn });
+export const generateToken = (
+  payload: object,
+  expiresInSec: number = 4 * 60 * 60
+) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: expiresInSec });
 };
 
 export const verifyToken = (token: string) => {
   try {
     return jwt.verify(token, JWT_SECRET);
   } catch (error) {
+    console.log(error)
     return null;
   }
 };
