127e8b18dc492a7482a2e99e4b5a010e2be93e49
Mukesh-Thidiff <mukesh.murugaiyan@gmail.com>
2025-12-06 12:08:01 +0530

    added new navigation bar

--- HomePage/components/Sections/NavigationTab.tsx
+++ HomePage/components/Sections/NavigationTab.tsx
@@ -1,9 +1,18 @@
 "use client";
-
-const  NavigationTab=()=> {
+interface NavigationTabProps {
+  setTabIndex: (index: number) => void;
+  tabIndex: number;
+}
+const NavigationTab: React.FC<NavigationTabProps> = ({
+  setTabIndex,
+  tabIndex,
+}) => {
+  const menuItems = ["About", "Resume", "Portfolio", "Contact", "Gallery"];
   return (
-    <button
-      className="
+    <>
+      {/* ✴---Desktop and web---✴ */}
+      <button
+        className="
         absolute top-0 right-0
         flex items-center gap-3     
         px-4 h-8 sm:px-6 sm:h-9
@@ -15,17 +24,58 @@ const  NavigationTab=()=> {
         duration-300 cursor-pointer
         hidden sm:flex
         z-[1]
-  "
-    >
-      {/* Desktop / Tablet Text */}
-      <span className="hidden sm:flex items-center gap-3">
-        <span className="cursor-pointer hover:text-white">About</span>
-        <span className="cursor-pointer hover:text-white">Resume</span>
-        <span className="cursor-pointer hover:text-white">Portfolio</span>
-        <span className="cursor-pointer hover:text-white">Contact</span>
-        <span className="cursor-pointer hover:text-white">Gallery</span>
-      </span>
-    </button>
+           "
+      >
+        <span className="hidden sm:flex items-center gap-3">
+          {menuItems.map((item, index) => (
+            <span
+              key={index}
+              className={`${
+                tabIndex == index ? "text-white" : ""
+              } cursor-pointer hover:text-white`}
+              onClick={() => {
+                setTabIndex(index);
+              }}
+            >
+              {item}
+            </span>
+          ))}
+        </span>
+      </button>
+
+      {/* ✴---Mobile Only---✴ */}
+      <div className="lg:hidden fixed bottom-0 left-0 w-full  backdrop-blur-md  z-50 sm:hidden">
+        <div className="flex justify-center items-center h-16 px-2">
+          <button
+            className="
+            text-xs font-medium
+            bg-gradient-to-r from-[hsl(190,82%,20%)] to-black
+            border border-[hsl(190,82%,72%)]
+            text-[hsl(190,82%,72%)]
+            px-4 py-2
+            duration-300
+            hover:text-white
+            transition-colors
+             rounded-[1.25rem]
+            flex gap-3
+            items-center justify-center
+          "
+          >
+            {menuItems.map((item, index) => (
+              <span
+                key={index}
+                onClick={() => {
+                  setTabIndex(index);
+                }}
+                className={`${tabIndex == index ? "text-white" : ""}`}
+              >
+                {item}
+              </span>
+            ))}
+          </button>
+        </div>
+      </div>
+    </>
   );
-}
-export default NavigationTab
+};
+export default NavigationTab;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
+import ProfileCard from "@/HomePage/components/ProfileCard/ProfileCard";
 import AboutSection from "@/HomePage/components/Sections/AboutSection";
 import HighlightsSection from "@/HomePage/components/Sections/HighlightsSection";
-import PortfolioSection from "@/HomePage/components/Sections/PortfolioSection";
 import NavigationTab from "@/HomePage/components/Sections/NavigationTab";
-import ProfileCard from "@/HomePage/components/ProfileCard/ProfileCard";
-import MobileNavigationTab from "@/HomePage/components/Sections/MobileNavigationTab";
+import PortfolioSection from "@/HomePage/components/Sections/PortfolioSection";
+import { useState } from "react";
 
 export default function Home() {
+  const [tabIndex, setTabIndex] = useState(0)
   return (
     <main className="bg-[#0b0b0d] text-white">
       <div className="w-full max-w-7xl mx-auto px-4 lg:px-10 py-12">
@@ -19,7 +20,7 @@ export default function Home() {
 
           {/* Right Scrollable Content */}
           <div className="flex-1  border border-white/15  rounded-[1.25rem]  backdrop-blur-sm">
-            <NavigationTab />
+            <NavigationTab setTabIndex={setTabIndex} tabIndex={tabIndex}/>
             <AboutSection />
             <HighlightsSection />
             <PortfolioSection />
@@ -30,7 +31,7 @@ export default function Home() {
         <div className="lg:hidden space-y-7 pb-28">
           <ProfileCard isMobile={true} />
           <div className="relative">
-            <NavigationTab />
+            <NavigationTab setTabIndex={setTabIndex} tabIndex={tabIndex}/>
             <AboutSection />
             <HighlightsSection />
             <PortfolioSection />
@@ -38,7 +39,6 @@ export default function Home() {
         </div>
       </div>
 
-        <MobileNavigationTab />
     </main>
   );
 }
